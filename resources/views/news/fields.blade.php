6d9699ed6363befb3b1defa353505e3fcf946b85
Arief Rufaidy <webdev2@mg.co.id>
2019-12-26 14:22:45 +0700

    news

--- resources/views/news/fields.blade.php
+++ resources/views/news/fields.blade.php
@@ -5,6 +5,12 @@
 </div>
 <div class="clearfix"></div>
 
+<!-- Source Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('title', 'Title:') !!}
+    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+</div>
+
 <!-- Content Field -->
 <div class="form-group col-sm-12 col-lg-12">
     {!! Form::label('content', 'Content:') !!}
